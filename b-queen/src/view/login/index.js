13d55b1e23cf3fd64491e47c0121d3505a6a5244
Ully Lopes <ullylopes@ymail.com>
2020-07-22 20:53:04 -0300

    initial

--- b-queen/src/view/login/index.js
+++ b-queen/src/view/login/index.js
@@ -14,23 +14,25 @@ function Login() {
     function logar() {
 
        firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
-          setMsgTipo('sucesso')
+         console.log('sucesso')
+          
        }).catch(erro => {
-          setMsgTipo('erro')
+         console.log('erro')
+          
        })
     }
 
     return(
         <div className='login-content d-flex align-items-certer'>
 
-        <form className="form-signin mx-auto">
-  <div class="text-center mb-4">
+        <form className="form-signin mx-auto ">
+  <div className="text-center mb-4">
     <img className="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
   </div>
 
 
-    <input onChange={(e) => setEmail(e.target.value)}  type="email" id="inputEmail" class="form-control my-3" placeholder="Email" />  
-    <input onChange={(e) => setSenha(e.target.value)} type="password" id="inputPassword" class="form-control my-3" placeholder="Senha" />
+    <input onChange={(e) => setEmail(e.target.value)}  type="email" id="inputEmail" className="form-control form-control-lg my-3" placeholder="Email" />  
+    <input onChange={(e) => setSenha(e.target.value)} type="password" id="inputPassword" className="form-control form-control-lg my-3" placeholder="Senha" />
   
 
   <button onClick={logar} className="btn btn-lg btn-block btn-login" type="button">Entrar</button>
