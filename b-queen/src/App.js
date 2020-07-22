02accc4efc1598edc9ebf5f8d0921ebd0a06e49f
Zaine Nascimento <amandazaine@outlook.com>
2020-07-22 12:59:25 -0300

    Primeiros passos

--- b-queen/src/App.js
+++ b-queen/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from './components/Button.js'
 import './App.css';
 
 function App() {
+
+  const fineOk = () => {
+    alert("Fine")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Zaine</p>
+      <br/>
+      <Button id="01" class="App-link" handleClick={fineOk} name="Ok"/>
+      <br/>
+      <Button id="02" handleClick={() =>{alert("Hii")}} name="Olá"/>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+</header>
+
+*/
