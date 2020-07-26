ae141578780bbf4ab9c9cfd41b653f2a6241ff1e
Zaine Nascimento <amandazaine@outlook.com>
2020-07-26 02:30:11 -0300

    Criando interface de registro

--- b-queen/src/view/register/index.js
+++ b-queen/src/view/register/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './register.css';
 import firebase from '../../config/firebase';
 import 'firebase/auth';
+import { Link } from 'react-router-dom';
 
 function Register(){  
     
@@ -9,36 +10,62 @@ function Register(){
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [confirmPassword, setConfirmPassword] = useState();
+    const [errorType, setErrorType] = useState();
+    const [errorMessage, setErrorMessage] = useState();
     
     function registerFirebase(){
-        alert("Cadastrou")
+        setErrorType(null)
+        
+        alert("Cadastro realizado!")
+        
+        /*
+        if(!email || !password){
+            setErrorType('error')
+            setErrorMessage('É necessário informar pelo menos email e senha para realizar o cadastro!')
+            return;
+        }else{
+
+            firebase.auth().createUserWithEmailAndPassword(email, password).then(
+
+            ).catch(error =>{
+                setErrorMessage('error')
+            })
+        }
+        */
     }
     
     return(
-        <div className="login-content d-flex align-items-center">
+        <div className="register-content d-flex">
+            <div className="form-register d-flex">
+                <form className="mx-auto text-center">
+                    
+                    <p className="edit-text-color" id="register-title">Cadastro</p>
+                    <div className="for-border">
+                        <input onChange={(e) => setName(e.target.value) } type="text" id="" className="form-control form-control-lg my-1" placeholder="Nome" />
+                        
+                        <input onChange={(e) => setEmail(e.target.value) } type="email" id="" className="form-control form-control-lg my-1" placeholder="Email" />
 
-            <form className="form-signin mx-auto ">
-                <div className="text-center mb-4">
-                    <img className="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"/>
-                </div>
+                        <input onChange={(e) => setPassword(e.target.value) } type="password" id="registration-password" className="form-control form-control-lg my-1" placeholder="Senha"/>
 
-                <p>Cadastro:</p>
-                <input onChange={(e) => setName(e.target.value) } type="text" id="" className="form-control form-control-lg my-3" placeholder="Nome" />
-                <input onChange={(e) => setEmail(e.target.value) } type="email" id="" className="form-control form-control-lg my-3" placeholder="Email" />
-                <input onChange={(e) => setPassword(e.target.value) } type="password" id="registration-password" className="form-control form-control-lg my-3" placeholder="Senha"/>
-                <input onChange={(e) => setConfirmPassword(e.target.value) } type="password" id="confirm-registration-password" className="form-control form-control-lg my-3" placeholder="Confirmar Senha"/>
+                        <input onChange={(e) => setConfirmPassword(e.target.value) } type="password" id="confirm-registration-password" className="form-control form-control-lg my-1" placeholder="Confirmar Senha"/>
 
-                <p>Setor:</p>
-                <input type="radio" id="radio-hall" className="mx-2" name="local" value="salao"/>
-                <label for="male">&#127828; Salão</label>
-                
-                <input type="radio" id="radio-kitchen" className="mx-2" name="local" value="cozinha"/>
-                <label for="female">&#128293; Cozinha</label>
+                        <p className="edit-text-color">Selecione o setor de trabalho:</p>
+                        <input type="radio" id="radio-hall" className="mx-2" name="local" value="salao"/>
+                        <label for="salao" className="edit-text-color">&#127828; Salão </label>
+                        
+                        <input type="radio" id="radio-kitchen" className="mx-2" name="local" value="cozinha"/>
+                        <label for="cozinha" className="edit-text-color">&#128293; Cozinha</label>
+                        <br/>
+                    </div>
+                    <button className="btn btn-register-page edit-text-color" type="button">Cancelar</button>
+                    <button onClick={registerFirebase} className="btn btn-register-page edit-text-color" type="button">Cadastrar</button>
 
-                <button className="btn btn-lg btn-block btn-register-page" type="button">Cancelar</button>
-                <button onClick={registerFirebase} className="btn btn-lg btn-block btn-register-page" type="button">Cadastrar</button>
+                    <div className="text-center">
+                        {errorType === "error" && <span><strong>{errorMessage}</strong></span>}
+                    </div>
 
-            </form>
+                </form>
+            </div>
         </div>
     )
 }
