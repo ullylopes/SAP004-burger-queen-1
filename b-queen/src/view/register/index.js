f87f45d79af150b585709646f2fdfe3b04e8cd9c
Zaine Nascimento <amandazaine@outlook.com>
2020-07-26 22:09:52 -0300

    Implementando registro com armazenamento de infos no banco de dados

--- b-queen/src/view/register/index.js
+++ b-queen/src/view/register/index.js
@@ -13,25 +13,45 @@ function Register(){
     const [errorType, setErrorType] = useState();
     const [errorMessage, setErrorMessage] = useState();
     
-    function registerFirebase(){
-        setErrorType(null)
-        
-        alert("Cadastro realizado!")
+    const registerFirebase = () => {
+        setErrorType(null)   
         
         /*
         if(!email || !password){
             setErrorType('error')
             setErrorMessage('É necessário informar pelo menos email e senha para realizar o cadastro!')
             return;
-        }else{
-
-            firebase.auth().createUserWithEmailAndPassword(email, password).then(
-
-            ).catch(error =>{
-                setErrorMessage('error')
-            })
-        }
+        }else{}
         */
+            firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+                      
+                firebase.firestore().collection('users').get().then((querySnapshot) => {
+                    const emailArray = [];
+                    querySnapshot.forEach((doc) => {
+                      emailArray.push(doc.data().email);
+                    })
+            
+                    const booleanEmail = [];
+                    for (let value of emailArray) {
+                      booleanEmail.push(value === email);
+                    }
+            
+                    const status = booleanEmail.indexOf(true);
+                    if (status === -1) {
+                      firebase.firestore().collection('users').doc().set({
+                        uid: firebase.auth().currentUser.uid,
+                        email: email,
+                        name: name
+                      })
+                      alert("Cadastro Efetuado!")                    
+                    }
+                  })
+                
+            }).catch(error =>{
+                setErrorMessage('error')
+                alert("Houve um erro ao cadastrar!")
+            })
+               
     }
     
     return(
@@ -41,20 +61,22 @@ function Register(){
                     
                     <p className="edit-text-color" id="register-title">Cadastro</p>
                     <div className="for-border">
-                        <input onChange={(e) => setName(e.target.value) } type="text" id="" className="form-control form-control-lg my-1" placeholder="Nome" />
+                        <input onChange={(e) => setName(e.target.value)} type="text" id="" className="form-control form-control-lg my-1" placeholder="Nome"/>
                         
-                        <input onChange={(e) => setEmail(e.target.value) } type="email" id="" className="form-control form-control-lg my-1" placeholder="Email" />
+                        <input onChange={(e) => setEmail(e.target.value)} type="email" id="" className="form-control form-control-lg my-1" placeholder="Email"/>
 
-                        <input onChange={(e) => setPassword(e.target.value) } type="password" id="registration-password" className="form-control form-control-lg my-1" placeholder="Senha"/>
+                        <input onChange={(e) => setPassword(e.target.value)} type="password" id="registration-password" className="form-control form-control-lg my-1" placeholder="Senha"/>
 
                         <input onChange={(e) => setConfirmPassword(e.target.value) } type="password" id="confirm-registration-password" className="form-control form-control-lg my-1" placeholder="Confirmar Senha"/>
 
                         <p className="edit-text-color">Selecione o setor de trabalho:</p>
+
                         <input type="radio" id="radio-hall" className="mx-2" name="local" value="salao"/>
                         <label for="salao" className="edit-text-color">&#127828; Salão </label>
                         
                         <input type="radio" id="radio-kitchen" className="mx-2" name="local" value="cozinha"/>
                         <label for="cozinha" className="edit-text-color">&#128293; Cozinha</label>
+
                         <br/>
                     </div>
                     <button className="btn btn-register-page edit-text-color" type="button">Cancelar</button>
@@ -63,7 +85,6 @@ function Register(){
                     <div className="text-center">
                         {errorType === "error" && <span><strong>{errorMessage}</strong></span>}
                     </div>
-
                 </form>
             </div>
         </div>
