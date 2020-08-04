7e4fa01788cdcb60ade0b65679064c184dddc831
Zaine Nascimento <amandazaine@outlook.com>
2020-08-03 23:36:46 -0300

    Começando a criar componente card para mostrar pedidos

--- b-queen/src/view/kitchen/index.js
+++ b-queen/src/view/kitchen/index.js
@@ -6,13 +6,42 @@ import Input from '../../components/Input'
 import { Link } from 'react-router-dom';
 import history from '../../history'
 import 'firebase/firestore';
+import Header from '../../components/Header';
+import Button from '../../components/Button';
+import Card from '../../components/Card';
 
 const Kitchen = () => {
 
 
 
     return(
-        <h1>Página da Cozinha</h1>
+        <div className="div-kitchen">
+            
+                <Header
+                    name1='Em Andamento'
+                    name2='Pedidos Concluídos'
+                />
+                <h3 className="for-title">PEDIDOS EM ANDAMENTO</h3>
+
+                <div className="d-flex ">
+                    <Card
+                            tableNumber='4'
+                            buttonTitle='PRONTO PARA SERVIR'
+                            client='Zaine'
+                            worker='Amanda'
+                    />
+
+                    <Card
+                            tableNumber='5'
+                            buttonTitle='PRONTO PARA SERVIR'
+                            client='Zaine'
+                            worker='Amanda'
+                    />
+                    
+                    
+                </div>
+            
+        </div>
     )
 
 }
