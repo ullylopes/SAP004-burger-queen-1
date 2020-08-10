b5fb06aa27636cd38e5fcab542df94fd1f5a8be7
Ully Lopes <ullylopes@ymail.com>
2020-08-10 15:29:21 -0300

    Trazer items do firebase

--- b-queen/src/components/Item/index.js
+++ b-queen/src/components/Item/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import './item.css';
 import Button from '../Button';
 
+
 const ItemSummary = (props) => {
     return (
 
-        <div className='item-summary-list'>
-            <div className='pt-3 pl-2'>
-                <span> NOME DO ITEM {props.item_name} </span>
+        <div className='item-summary-list w-100 my-sm-2'>
+            <span className='font-style'> NOME DO ITEM{props.item_name} </span>
+            <div className=''>
                 <button className='btn-quantity'>-</button>
                 <span className='counter'>0{props.quantity}</span>
                 <button className='btn-quantity'>+</button>
+
+                <span className='justify-content-end font-style'> R${props.price} </span>
+
+                <Button
+                    name='excluir'
+                    className='flex-row-reverse' />
             </div>
         </div>
+
+
 
     )
 }
